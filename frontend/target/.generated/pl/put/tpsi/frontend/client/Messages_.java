d21841e6edf6b987d008c32945966337fd2f5ec6
oskaro89 <oskaro89@o2.pl>
2012-05-13 14:51:23 +0200

    pierwszy raz

---
+++ frontend/target/.generated/pl/put/tpsi/frontend/client/Messages_.java
@@ -0,0 +1,12 @@
+package pl.put.tpsi.frontend.client;
+
+public class Messages_ implements pl.put.tpsi.frontend.client.Messages {
+  
+  public java.lang.String nameField() {
+    return "Enter your name";
+  }
+  
+  public java.lang.String sendButton() {
+    return "Send";
+  }
+}
